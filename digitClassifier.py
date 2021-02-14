a90d7cc8bef73d9141ed949202732dfa14119d47
Pranav Singhal <iit2019050@iiita.ac.in>
2021-02-14 14:55:44 +0530

    trained the model and wrote the testing script

--- digitClassifier.py
+++ digitClassifier.py
@@ -4,6 +4,9 @@ import cv2
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from utility import *
+from keras_preprocessing.image import ImageDataGenerator
+from keras.utils.np_utils import to_categorical
+import pickle
 print('Starting.................')
 myList = os.listdir(path="data")
 print('Number of Classes=',len(myList))
@@ -43,12 +46,12 @@ for i in range(noClass):
     numofSamples.append(len(np.where(Y_train==str(i))[0]))
 print('Number of Samples for a class in Training Set = ',numofSamples)
 
-plt.figure(figsize=(10,5))
-plt.bar(range(0,noClass),numofSamples)
-plt.title('Number of Images in the training set for each class.')
-plt.xlabel('Class Id')
-plt.ylabel('Number of Images')
-plt.show()
+# plt.figure(figsize=(10,5))
+# plt.bar(range(0,noClass),numofSamples)
+# plt.title('Number of Images in the training set for each class.')
+# plt.xlabel('Class Id')
+# plt.ylabel('Number of Images')
+# plt.show()
 
 X_train = np.array(list(map(preprocessing,X_train)))
 X_test = np.array(list(map(preprocessing,X_test)))
@@ -57,8 +60,53 @@ X_val = np.array(list(map(preprocessing,X_val)))
 X_train=X_train.reshape(X_train.shape[0],X_train.shape[1],X_train.shape[2],1)
 X_test=X_test.reshape(X_test.shape[0],X_test.shape[1],X_test.shape[2],1)
 X_val=X_val.reshape(X_val.shape[0],X_val.shape[1],X_val.shape[2],1)
-print(X_train.shape)
-img = X_train[67]
-img = cv2.resize(img,(300,300))
-cv2.imshow(str(Y_train[67]),img)
-cv2.waitKey(0)
+# print(X_train.shape)
+# img = X_train[67]
+# img = cv2.resize(img,(300,300))
+# cv2.imshow(str(Y_train[67]),img)
+# cv2.waitKey(0)
+dataGenerator = ImageDataGenerator(width_shift_range=0.1,height_shift_range=0.1,zoom_range=0.2,shear_range=0.1,rotation_range=10)
+dataGenerator.fit(X_train) # to know more about the images to be generated
+
+Y_train=to_categorical(Y_train,noClass)
+Y_val=to_categorical(Y_val,noClass)
+Y_test=to_categorical(Y_test,noClass)
+
+print(len(X_train))
+print(len(Y_train))
+model = myModel()
+print(model.summary())
+
+history = model.fit_generator(dataGenerator.flow(X_train,Y_train,
+                                 batch_size=50),
+                                 steps_per_epoch=2000,
+                                 epochs=10,
+                                 validation_data=(X_val,Y_val),
+                                 shuffle=1)
+
+#### PLOTTING THE RESULTS
+plt.figure(1)
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.legend(['training','validation'])
+plt.title('Loss')
+plt.xlabel('epoch')
+plt.figure(2)
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.legend(['training','validation'])
+plt.title('Accuracy')
+plt.xlabel('epoch')
+plt.show()
+
+#### EVALUATE USING TEST IMAGES
+score = model.evaluate(X_test,Y_test,verbose=0)
+print('Test Score = ',score[0])
+print('Test Accuracy =', score[1])
+
+#### SAVE THE TRAINED MODEL
+model.save('CNN_model.h5')
+pickle_out= open("model_trained.p", "wb")
+pickle.dump(model,pickle_out)
+pickle_out.close()
+print('Model Saved!')
--- utility.py
+++ utility.py
@@ -1,8 +1,41 @@
 import cv2
 import numpy as np
-
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
+from keras.layers import Dropout,Flatten
+from keras.layers.convolutional import Conv2D,MaxPooling2D
+noClass=10
+imageDimensions=(32,32,3)
 def preprocessing(img):
     img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     img = cv2.equalizeHist(img)
     img=img/255 #normalise the grayscale image
     return img
+
+# model for the training based on CNN model
+def myModel():
+    noOfFilters = 60
+    sizeOfFilter1 = (5, 5)
+    sizeOfFilter2 = (3, 3)
+    sizeOfPool = (2, 2)
+    noOfNodes = 500
+
+    model = Sequential()
+    model.add((Conv2D(noOfFilters, sizeOfFilter1, input_shape=(imageDimensions[0],
+                                                               imageDimensions[1], 1), activation='relu')))
+    model.add((Conv2D(noOfFilters, sizeOfFilter1, activation='relu')))
+    model.add(MaxPooling2D(pool_size=sizeOfPool))
+    model.add((Conv2D(noOfFilters // 2, sizeOfFilter2, activation='relu')))
+    model.add((Conv2D(noOfFilters // 2, sizeOfFilter2, activation='relu')))
+    model.add(MaxPooling2D(pool_size=sizeOfPool))
+    model.add(Dropout(0.5))
+
+    model.add(Flatten())
+    model.add(Dense(noOfNodes, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(noClass, activation='softmax'))
+
+    model.compile(Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
